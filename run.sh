d95dd8f0e56c2c375cab3136a4d9df24fba9298c
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2018-08-21 15:47:21 -0300

    Script para ejecutar DEMPS

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#time ./demps -m /home/rsolar/mobility_models/osm-data/valparaiso.osrm -s input/settings.json -i input/initial_zones.geojson -r input/reference_zones.geojson -p input/reference_point.geojson -a input/area.geojson
 
+#time ./demps -m input/iquique/area.osrm -s input/iquique/settings.json -i input/iquique/initial_zones.geojson -r input/iquique/reference_zones.geojson -p input/iquique/reference_point.geojson
 
-time ./demps -m input/iquique/area.osrm -s input/iquique/settings.json -i input/iquique/initial_zones.geojson -r input/iquique/reference_zones.geojson -p input/iquique/reference_point.geojson
+time ./demps  -s demps.config
