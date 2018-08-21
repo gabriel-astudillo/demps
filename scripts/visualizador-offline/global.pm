e5cb2580010a54153917fd6cf2b7cbd321a59199
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2018-08-21 16:07:48 -0300

    Add files via upload

--- scripts/visualizador-offline/global.pm
+++ scripts/visualizador-offline/global.pm
@@ -5,7 +5,7 @@ package global;
 $sim_img_results_directory = "mapimages_sim/";
 #my $sim_img_results_directory = "../../results/";
 
-$plot_latlong_path_rel = "../plot-latlong.pl";
+$plot_latlong_path_rel = "../plot-latlong/plot-latlong.pl";
 $plot_latlong_options  = "-m iquique -s 2";
 
 1;
