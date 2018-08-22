d3be3134dd3c0feafcedc7c0c84c5766401888d2
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2018-08-21 23:03:08 -0300

    Script para crear GIF animado

---
+++ scripts/visualizador-offline/anim_gif.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dir_img="mapimages_sim"
+temp=$$
+mkdir $temp
+
+files=$(ls -1 $dir_img/*.png | cut -d '/' -f 2 | sort -n)
+
+ii=0
+for i in $files; do 
+	index=$(printf "%05d" $ii)
+	convert $dir_img/$i $temp/$index.gif; 
+	ii=$((ii+1))
+done
+
+convert -delay 10 -loop 0 $temp/*.gif anim.gif
+
+rm -rf $temp
