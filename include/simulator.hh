827f2d6fa7e9b002f0d8d1a5639b0075fe39e757
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2018-09-05 14:16:45 -0300

    Add files via upload

--- include/simulator.hh
+++ include/simulator.hh
@@ -6,33 +6,31 @@
 #include <router.hh>
 #include <environment.hh>
 
-class Simulator
-{
+class Simulator{
 private:
+	static const std::hash<std::string> _hash;
+	json                                _fsettings;
 
-    static const std::hash<std::string> _hash;
-    json                                _fsettings;
-
-    Router                              _router;
+	Router                              _router;
 	Environment                          env;
-    LocalCartesian                      _projector;
-    std::vector<GeoCoords>              _reference_points;
-    std::vector<Zone>                   _initial_zones;
-    std::vector<Zone>                   _reference_zones;
-    std::vector<Agent>                  _agents;
+	LocalCartesian                      _projector;
+	std::vector<GeoCoords>              _reference_points;
+	std::vector<Zone>                   _initial_zones;
+	std::vector<Zone>                   _reference_zones;
+	std::vector<Agent>                  _agents;
 
-    std::map<uint32_t,std::list<Point2D>> _routes;
+	std::map<uint32_t,std::list<Point2D>> _routes;
 
-    void run(const uint32_t&);
-    void save(const uint32_t&);
+	void run(const uint32_t&);
+	void save(const uint32_t&);
 
 public:
-    Simulator(void);
-    Simulator(const json&,const json&,const json&,const json&,const std::string&);
+	Simulator(void);
+	Simulator(const json&,const json&,const json&,const json&,const std::string&);
 
-    void run(void);
-    void calibrate(void);
+	void run(void);
+	void calibrate(void);
 
-    ~Simulator(void);
+	~Simulator(void);
 };
 #endif
