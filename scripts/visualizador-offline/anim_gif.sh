329d438788a9130212e6678ee8df8d5f1e5f3663
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2018-09-05 15:39:40 -0300

    Add files via upload

--- scripts/visualizador-offline/anim_gif.sh
+++ scripts/visualizador-offline/anim_gif.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-dir_img="mapimages_sim"
+# Crea un archivo GIF animado a partir de las imágenes PNG 
+# de la simulación, generados por el script "make_maps.pl"
 
-#temp=$$
-#mkdir $temp
-#files=$(ls -1 $dir_img/*.png | cut -d '/' -f 2 | sort -n)
-#
-#ii=0
-#for i in $files; do 
-#	index=$(printf "%05d" $ii)
-#	convert $dir_img/$i $temp/$index.gif; 
-#	ii=$((ii+1))
-#done
-#
-#convert -delay 10 -loop 0 $temp/*.gif anim.gif
+# DEPENDENCIAS:
+#    comando "convert" : ImageMagick
+#    comando "gifsicle": https://www.lcdf.org/gifsicle/, 
+#                        debian: apt-get install gifsicle
+
+
+dir_img="mapimages_sim"
 
 echo "Creando gif"
 convert -delay 10 -loop 0 $dir_img/*.png anim.gif
