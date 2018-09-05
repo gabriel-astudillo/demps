b6e183f908696f62c848bd6a5f7113a8025a3821
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2018-09-05 17:04:23 -0300

    Add files via upload

--- scripts/visualizador-offline/run-make_maps.sh
+++ scripts/visualizador-offline/run-make_maps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "Eliminando imágenes anteriores..."
+rm -f mapimages_sim/*
 
-echo "Creando imágenes a partir de los logs de la simulación"
+echo "Creando imágenes a partir de los logs de la simulación..."
 ./make_maps.pl -r ../../results
 
 ./anim_gif.sh
