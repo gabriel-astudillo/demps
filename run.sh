31b3053b042e1bcd2da37b6ce7ac5ed025272726
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2018-10-03 16:22:47 -0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -2,13 +2,32 @@
 
 #### CONFIGURACION ####
 DEMPS_PATH=./demps 
-DEMPS_OPTS="-s demps.config"
-RESULTS_FILES="./results/*"
+
+DEMPS_CONFIG_FILE="demps-iquique.config"
+#DEMPS_CONFIG_FILE="demps-curaumaPUCV.config"
+#DEMPS_CONFIG_FILE="demps-vdm.config"
+
+# command-line JSON processor
+# https://stedolan.github.io/jq/
+JQ_PATH=/usr/bin/jq
+
+if [[ ! -e $JQ_PATH ]]; then
+	echo "JQ no está instalado en $JQ_PATH."
+	echo "https://stedolan.github.io/jq/"
+	exit
+fi
+
+DEMPS_OPTS="-s $DEMPS_CONFIG_FILE"
+RESULTS_DIR=$(cat $DEMPS_CONFIG_FILE | $JQ_PATH -r '.output."filesim-path"')
+RESULTS_FILES="$RESULTS_DIR/*"
 
 RM_CMD="$(which rm) -f"
 
 #### MAIN ####
+printf "DEMPS_CONFIG_FILE=%s\n" "$DEMPS_CONFIG_FILE"
+
 echo "Eliminando resultados anteriores..."
 $RM_CMD $RESULTS_FILES
+
 echo "Ejecutando DEMPS..."
 time $DEMPS_PATH $DEMPS_OPTS
