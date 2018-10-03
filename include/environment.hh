ca61c78844d2f542974d2a41c951b1e177384273
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2018-10-02 22:22:03 -0300

    Add files via upload

--- include/environment.hh
+++ include/environment.hh
@@ -9,39 +9,39 @@ class Router;
 
 class Environment{
 public:
-    std::map<uint32_t,std::list<Point2D>> _routes;
+	std::map<uint32_t,std::list<Point2D>> _routes;
 
 private:
 	Router _router;
-	
+
 	std::vector<Agent> _vAgents;
 	double _xMin, _xMax, _yMin, _yMax;
 	double _mapWidth, _mapHeight;
-	
+
 	// Largo del cuadrante (en [m])
 	uint32_t _quadSize;
-	
+
 	//Cantidad de cuadrantes en el eje X e Y
 	uint32_t _quadX, _quadY;
 
 public:
-    Environment(void);
+	Environment(void);
 	Environment(double xMin,double xMax, double yMin, double yMax, uint32_t quadSize);
 
 	Environment(const Environment&);
-    Environment(const std::vector<Agent>&);
-    ~Environment(void);
-    Environment& operator=(const Environment&);
+	Environment(const std::vector<Agent>&);
+	~Environment(void);
+	Environment& operator=(const Environment&);
 
 	void addAgents(const std::vector<Agent> &_vAgents);
 	uint32_t getTotalAgents();
 	Agent* getAgent(uint32_t id);
 	std::vector<Agent> getAgents();
-	
-    Agent::Neighbors neighbors_of(const Agent&,const double&,const model_t&);
-    
+
+	Agent::Neighbors neighbors_of(const Agent&,const double&,const model_t&);
+
 	void updateAgents();
-	
+
 	void setRouter(const json &_freference_point,const std::string &_map_osrm);
 	Router getRouter(void);
 };
