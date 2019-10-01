866e89f3b076d6a4050039223064665bfa57c963
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2019-09-30 21:52:18 -0300

    Delete run-make_maps.sh

--- scripts/visualizador-offline/run-make_maps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Eliminando imágenes anteriores..."
-rm -f mapimages_sim/*
-
-echo "Creando imágenes a partir de los logs de la simulación..."
-./make_maps.pl -r ../../results
-
-./anim_gif.sh
-
