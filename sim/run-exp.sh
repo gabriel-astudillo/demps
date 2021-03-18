0f06c7a5c5cdcf22a7cf42c5e79ec781ad7313f5
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2021-03-18 10:47:33 -0300

    Update run-exp.sh

--- sim/run-exp.sh
+++ sim/run-exp.sh
@@ -13,7 +13,7 @@ for i in $(seq 1 $TOTAL_EXP)
 do
 	DIROUTPUT=$(printf "%s/%05d" $DIR_OUTPUT $i)
 	#DIROUTPUT es de la forma DIR_OUTPUT/N, siendo N un número de 5 digitos
-	#Por ejemplo: output/iquique_test_exp/00025
+	#Por ejemplo: output/iquique_test_exp/00005
 	
 	CMD="./run.sh -c $CONFIG_FILE -o $DIROUTPUT -e $i"
 	echo "======"
