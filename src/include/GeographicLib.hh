3bb2eb05f2cc28d89fb1732c2d011434eea8271c
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2024-10-30 11:02:34 -0300

    Delete src directory

--- src/include/GeographicLib.hh
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <GeographicLib/Geodesic.hpp>
-#include <GeographicLib/Constants.hpp>
-#include <GeographicLib/GeoCoords.hpp>
-#include <GeographicLib/LocalCartesian.hpp>
-using namespace GeographicLib;
